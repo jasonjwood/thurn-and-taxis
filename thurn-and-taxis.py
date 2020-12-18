2da084f7325f19d6492ba9aba4e6f29360c16483
Jason Wood <jason.j.wood@gmail.com>
2020-12-17 22:36:17 -0500

    City definition and raw input files

--- thurn-and-taxis.py
+++ thurn-and-taxis.py
@@ -1,6 +1,48 @@
+cities  =   {   'basel': {'colour': 'lightblue', 'players': [], 'roads': ['freiburg', 'zurich']},
+                'zurich': {'colour': 'lightblue', 'players': [], 'roads': ['basel', 'freiburg', 'sigmaringen', 'kempten']},
+                'freiburg': {'colour': 'purple', 'players': [], 'roads': ['basel', 'zurich', 'sigmaringen', 'carlsruhe']},
+                'carlsruhe': {'colour': 'purple', 'players': [], 'roads': ['freiburg', 'mannheim', 'stuttgart']},
+                'mannheim': {'colour': 'purple', 'players': [], 'roads': ['carlsruhe', 'wurzburg', 'stuttgart']},
+                'sigmaringen': {'colour': 'green', 'players': [], 'roads': ['freiburg', 'zurich', 'kempten', 'ulm', 'stuttgart']},
+                'ulm': {'colour': 'lightgreen', 'players': [], 'roads': ['sigmaringen', 'stuttgart', 'kempten', 'ingolstadt', 'augsberg']},
+                'stuttgart': {'colour': 'lightgreen', 'players': [], 'roads': ['sigmaringen', 'ulm', 'nurnburg', 'ingolstadt', 'wurzburg', 'mannheim', 'carlsruhe']},
+                'innsbruck': {'colour': 'blue', 'players': [], 'roads': ['kempten', 'augsberg', 'munchen', 'salzburg']},
+                'wurzburg': {'colour': 'grey', 'players': [], 'roads': ['mannheim', 'stuttgart', 'nurnburg']},
+                'nurnburg': {'colour': 'grey', 'players': [], 'roads': ['wurzburg', 'stuttgart', 'ingolstadt', 'regensburg', 'pilsen']},
+                'regensburg': {'colour': 'grey', 'players': [], 'roads': ['nurnburg', 'ingolstadt', 'passau', 'pilsen', 'munchen']},
+                'ingolstadt': {'colour': 'grey', 'players': [], 'roads': ['nurnburg', 'regensburg', 'augsburg', 'ulm', 'munchen', 'stuttgart']},
+                'augsburg': {'colour': 'grey', 'players': [], 'roads': ['munchen', 'ingolstadt', 'innsbruck', 'ulm', 'kempten']},
+                'kempten': {'colour': 'grey', 'players': [], 'roads': ['augsburg', 'ingolstadt', 'sigmaringen', 'ulm', 'zurich']},
+                'passau': {'colour': 'grey', 'players': [], 'roads': ['salzburg', 'linz', 'munchen', 'regensburg']},
+                'munchen': {'colour': 'grey', 'players': [], 'roads': ['salzburg', 'passau', 'ingolstadt', 'regensburg', 'augsburg', 'innsbruck']},
+                'salzburg': {'colour': 'red', 'players': [], 'roads': ['innsbruck', 'linz', 'munchen', 'passau']},
+                'linz': {'colour': 'red', 'players': [], 'roads': ['budweis', 'salzburg', 'passau']},
+                'budweis': {'colour': 'orange', 'players': [], 'roads': ['pilsen', 'linz']},
+                'pilsen': {'colour': 'orange', 'players': [], 'roads': ['budweis', 'lodz', 'nurnburg', 'regensburg']},
+                'lodz': {'colour': 'orange', 'players': [], 'roads': ['pilsen']}
+              }
+
+
+
+
+def read_game_state_from_file(){
+
+}
+
 
 def main() {
-test
+
+    # Read game state from file
+    read_game_state_from_file()
+
+
+    # Choose move
+
+
+
+
+
+
 }
 
 main()
